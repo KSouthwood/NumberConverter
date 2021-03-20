20a498e7b9a184dd4c43c11c024f02141dcc90d2
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2021-03-20 15:36:15 -0400

    Complete Stage 2
    Present the user with a menu to choose to convert from or to a decimal number. They also have the option to exit the program.
    
    Depending on choice, get the decimal number and base to convert to, or get the source number and source base to convert from. In either case, perform the necessary conversion and output the result.

--- converter/Main.kt
+++ converter/Main.kt
@@ -1,15 +1,43 @@
 package converter
 
+const val digits = "0123456789ABCDEF"
+
 fun main() {
-    print("Enter number in decimal system: ")
-    val num = readLine()!!.toInt()
-    print("Enter target base: ")
-    val base = readLine()!!.toInt()
+    var command = ""
+
+    while (command != "/exit") {
+        print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ")
+        command = readLine()!!
+        if (command == "/from") {
+            fromDecimal()
+        }
+        if (command == "/to") {
+            toDecimal()
+        }
+    }
+}
+
+fun fromDecimal() {
+    val num = promptForInput("Enter a number in decimal system: ").toInt()
+    val base = promptForInput("Enter the target base: ").toInt()
     println("Conversion result: ${convertDecimal(num, base)}")
 }
 
+fun toDecimal() {
+    val num = promptForInput("Enter source number: ").toUpperCase()
+    val base = promptForInput("Enter source base: ").toInt()
+    println("Conversion to decimal result: ${convertNonDecimal(num, base)}")
+}
+
+fun promptForInput(msg: String) : String {
+    print(msg)
+    return readLine()!!
+}
+
+/**
+ * Perform the conversion from decimal to the requested number base (radix)
+ */
 fun convertDecimal(decimal: Int, base: Int): String {
-    val digits = "0123456789ABCDEF"
     var conversion = ""
     var quotient = decimal
 
@@ -20,4 +48,19 @@ fun convertDecimal(decimal: Int, base: Int): String {
     }
 
     return conversion.reversed()
+}
+
+/**
+ * Perform the conversion from the supplied base back to decimal
+ */
+fun convertNonDecimal(num: String, base: Int): Int {
+    var conversion = 0
+    var value = 1
+
+    for (index in num.lastIndex downTo 0) {
+        conversion += digits.indexOf(num[index]) * value
+        value *= base
+    }
+
+    return conversion
 }
